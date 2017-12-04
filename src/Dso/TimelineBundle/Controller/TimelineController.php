3a1e896b4b651bda8734fa384ac2220967484b65
tiranflorin <tiran.florin@gmail.com>
2017-12-05 01:29:18 +0200

    Merge branch 'master' of https://github.com/tiranflorin/Deep-Skies-Symfony
    Conflicts:
    	src/Dso/TimelineBundle/Controller/TimelineController.php

--- src/Dso/TimelineBundle/Controller/TimelineController.php
+++ src/Dso/TimelineBundle/Controller/TimelineController.php
@@ -2,10 +2,12 @@
 
 namespace Dso\TimelineBundle\Controller;
 
+use Doctrine\ORM\AbstractQuery;
 use Dso\ObservationsLogBundle\Entity\ObsList;
 use Dso\PlannerBundle\Services\SQL\MySqlService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Connection;
 
 class TimelineController extends Controller
 {
@@ -23,7 +25,7 @@ class TimelineController extends Controller
             obs_lists.name as 'obsListName',
             obs_lists.equipment as 'obsEquipment',
             obs_lists.conditions as 'obsConditions'
-        FROM `" . $this->container->getParameter('database_name') . "`.timeline_events AS `timeline`
+        FROM `deep-skies-sym`.timeline_events AS `timeline`
         LEFT JOIN `obs_lists`
             ON `timeline`.`obs_list_id` = `obs_lists`.`id`
         LEFT JOIN `fos_user`
@@ -75,6 +77,138 @@ class TimelineController extends Controller
         return $this->render(
             'DsoTimelineBundle:Timeline:objects_attached_to_list.html.twig',
             array('loggedObjects' => $loggedObjects)
+        );
+    }
+
+    public function obsListItemsTextFormatAction($obsListId)
+    {
+        /** @var ObsList $observingList */
+        $observingList = $this->getDoctrine()
+            ->getRepository('DsoObservationsLogBundle:ObsList')
+            ->find($obsListId);
+        if (!$observingList) {
+            throw $this->createNotFoundException(
+                'No observing list found!'
+            );
+        }
+        if ($observingList->getVisibilityLevel() === 'private') {
+            throw $this->createNotFoundException(
+                'No observing list found!'
+            );
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT d
+            FROM DsoObservationsLogBundle:LoggedObject l
+            JOIN DsoObservationsLogBundle:DeepSkyItem d
+              WITH d.id = l.objId
+            WHERE l.listId = :list_id'
+        )->setParameter('list_id', $obsListId);
+
+        $loggedObjects = $query->getResult();
+
+        return $this->render(
+            'DsoTimelineBundle:Timeline:objects_attached_to_list_text.html.twig',
+            array('loggedObjects' => $loggedObjects)
+        );
+    }
+
+    public function shareableObsListAction($observingListId) {
+        /** @var ObsList $observingList */
+        $observingList = $this->getDoctrine()
+            ->getRepository('DsoObservationsLogBundle:ObsList')
+            ->find($observingListId);
+        if (!$observingList) {
+            throw $this->createNotFoundException(
+                'No observing list found!'
+            );
+        }
+        if ($observingList->getVisibilityLevel() === 'private') {
+            throw $this->createNotFoundException(
+                'No observing list found!'
+            );
+        }
+
+        /** @var MySqlService $mysql_service */
+        $mysqlService = $this->get('dso_planner.mysql_service');
+        $sql = "
+        SELECT
+            fos_user.username,
+            fos_user.id,
+            timeline.added_on as 'addedOn',
+            timeline.name as 'eventName',
+            obs_lists.id as 'obsListId',
+            obs_lists.name as 'obsListName',
+            obs_lists.equipment as 'obsEquipment',
+            obs_lists.conditions as 'obsConditions'
+        FROM `timeline_events` AS `timeline`
+        LEFT JOIN `obs_lists`
+            ON `timeline`.`obs_list_id` = `obs_lists`.`id`
+        LEFT JOIN `fos_user`
+            ON `timeline`.`user_id` = `fos_user`.`id`
+        WHERE `obs_lists`.`id` = $observingListId
+        ORDER BY `timeline`.`id` DESC
+        ";
+        $stmt = $mysqlService->getConn()->executeQuery($sql);
+        $results = $stmt->fetchAll();
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT d.id
+            FROM DsoObservationsLogBundle:LoggedObject l
+            JOIN DsoObservationsLogBundle:DeepSkyItem d
+              WITH d.id = l.objId
+            WHERE l.listId = :list_id'
+        )->setParameter('list_id', $observingListId);
+
+        $loggedObjects = $query->getResult(AbstractQuery::HYDRATE_ARRAY);
+        $objIds = array_map(function($loggedObject) {
+            return $loggedObject['id'];
+        }, $loggedObjects);
+
+        $sql = "
+        SELECT DISTINCT
+            logged.observedAt,
+            logged.comment,
+            source.name as `Name1`,
+            source.other_name as `Name2`,
+            source.type as `ObjType`,
+            source.constellation as `Constellation`,
+            source.mag as `ObjMagnitude`,
+            source.size_min as `ObjMinSize`,
+            source.size_max as `ObjMaxSize`,
+            source.ngc_description as `Ngc_desc`,
+            source.notes as `Other_notes`,
+            IFNULL(img.thumb, 'no_image_available.png') as `thumb`,
+            IFNULL(img.full_size, 'no_image_available_large.png') as `full_size`
+        FROM  `logged_objects` as logged
+        LEFT JOIN `object`  as source
+            ON logged.obj_id = source.id
+        LEFT JOIN `image_paths` as img
+            ON img.object_id = source.id
+
+        WHERE
+            logged.list_id = ? AND
+            logged.obj_id IN (?)
+        ";
+        $stmt = $mysqlService->getConn()->executeQuery(
+            $sql,
+            array(
+                $observingListId,
+                $objIds
+            ),
+            array(\PDO::PARAM_INT, Connection::PARAM_INT_ARRAY)
+        );
+
+        $loggedObjects = $stmt->fetchAll();
+
+        return $this->render(
+            'DsoTimelineBundle:Timeline:shareable_observing_list.html.twig',
+            array(
+                'event' => reset($results),
+                'loggedObjects' => $loggedObjects
+            )
         );
     }
 }
